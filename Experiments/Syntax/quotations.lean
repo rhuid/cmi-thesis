2d8ededeacb3ababfcc5f44f11cd182d1a7bd484
Ronald Huidrom <ronhuidrom@gmail.com>
2026-05-14 02:40:36 +0530

    quotes?

--- Experiments/Syntax/quotations.lean
+++ Experiments/Syntax/quotations.lean
@@ -1,4 +1,4 @@
-set_option diagnostics true
+-- set_option diagnostics true
 
 #check ``Nat.succ
 
@@ -30,3 +30,61 @@ def newthing := Lean.Name.num `crap 10
 #print Lean.Syntax.node
 
 #print Lean.Syntax
+
+#eval repr ``Nat.succ
+#eval repr (Nat.succ 4)
+
+-- Syntax
+
+declare_syntax_cat mystr
+syntax "mystring" str : mystr
+
+macro_rules
+| `(mystr| mystring $st) => do `(command| #eval $st)
+
+syntax "MyTerm" : term
+
+#check `(term| MyTerm)
+
+
+declare_syntax_cat toy
+syntax "hello" : toy
+
+#check `(toy| hello)
+
+
+declare_syntax_cat my_clause
+declare_syntax_cat my_long_clause
+
+#check Lean.Parser.Category.my_clause
+
+syntax "variable" term : my_clause
+syntax "variable" ident ":" term "=" term  : my_long_clause
+
+
+
+
+macro_rules
+| `(my_clause| variable $term) => do `(command| #print $term)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/-
+- At any point, there is only one value of the type Lean.Syntax which represents the CST of the entire file after the parser has done its job?
+-
+
+-/
